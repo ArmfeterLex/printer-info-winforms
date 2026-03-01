f3383f0a52d8f57b8a2fdbdbfbf51cd067634786
ArmfeterLex <alexromanenkobeats@yandex.ru>
2026-03-01 18:56:18 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace PrinterApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.butPrint = new System.Windows.Forms.Button();
+            this.butPreview = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(360, 244);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Список";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // butPrint
+            // 
+            this.butPrint.Location = new System.Drawing.Point(230, 262);
+            this.butPrint.Name = "butPrint";
+            this.butPrint.Size = new System.Drawing.Size(142, 39);
+            this.butPrint.TabIndex = 2;
+            this.butPrint.Text = "Печать";
+            this.butPrint.UseVisualStyleBackColor = true;
+            this.butPrint.Click += new System.EventHandler(this.butPrint_Click);
+            // 
+            // butPreview
+            // 
+            this.butPreview.Location = new System.Drawing.Point(230, 307);
+            this.butPreview.Name = "butPreview";
+            this.butPreview.Size = new System.Drawing.Size(142, 37);
+            this.butPreview.TabIndex = 3;
+            this.butPreview.Text = "Preview";
+            this.butPreview.UseVisualStyleBackColor = true;
+            this.butPreview.Click += new System.EventHandler(this.butPreview_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 356);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.butPrint);
+            this.Controls.Add(this.butPreview);
+            this.Name = "MainForm";
+            this.Text = "Принтеры";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button butPrint;
+        private System.Windows.Forms.Button butPreview;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PrinterApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            foreach (string printerName in PrinterSettings.InstalledPrinters)
+            {
+                textBox1.AppendText("Принтер: " + printerName + "\r\n");
+
+                try
+                {
+                    PrinterSettings printer = new PrinterSettings();
+                    printer.PrinterName = printerName;
+
+                    if (printer.IsValid)
+                    {
+                        textBox1.AppendText("  Поддерживаемые разрешения:\r\n");
+                        foreach (PrinterResolution resolution in printer.PrinterResolutions)
+                        {
+                            textBox1.AppendText("    " + resolution + "\r\n");
+                        }
+
+                        textBox1.AppendText("  Поддерживаемые размеры бумаги:\r\n");
+                        foreach (PaperSize size in printer.PaperSizes)
+                        {
+                            if (Enum.IsDefined(size.Kind.GetType(), size.Kind))
+                            {
+                                textBox1.AppendText("    " + size.PaperName + "\r\n");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        textBox1.AppendText("  Принтер недействителен.\r\n");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    textBox1.AppendText("  Ошибка при получении информации о принтере: " + ex.Message + "\r\n");
+                }
+            }
+        }
+
+        private void butPrint_Click(object sender, EventArgs e)
+        {
+            PrintDocument doc = new PrintDocument();
+            doc.PrintPage += this.Doc_PrintPage;
+
+            PrintDialog dlgSettings = new PrintDialog();
+            dlgSettings.Document = doc;
+
+            if (dlgSettings.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    doc.Print();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при печати: " + ex.Message);
+                }
+            }
+        }
+
+        private void butPreview_Click(object sender, EventArgs e)
+        {
+            PrintDocument doc = new PrintDocument();
+            doc.PrintPage += this.Doc_PrintPage;
+
+            PrintPreviewDialog dlgPreview = new PrintPreviewDialog();
+            dlgPreview.Document = doc;
+
+            dlgPreview.ShowDialog();
+        }
+
+        private void Doc_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            using (Font font = new Font("Arial", 30))
+            {
+                float x = e.MarginBounds.Left;
+                float y = e.MarginBounds.Top;
+                float lineHeight = font.GetHeight(e.Graphics);
+
+                for (int i = 1; i <= 5; i++)
+                {
+                    e.Graphics.DrawString("Это строчка номер " + i.ToString(), font, Brushes.Black, x, y);
+                    y += lineHeight;
+                }
+
+                y += lineHeight;
+
+                try
+                {
+                    string imagePath = Path.Combine(Application.StartupPath, "test.jpg");
+                    if (File.Exists(imagePath))
+                    {
+                        Image image = Image.FromFile(imagePath);
+                        e.Graphics.DrawImage(image, x, y);
+                    }
+                    else
+                    {
+                        e.Graphics.DrawString("Изображение test.jpg не найдено", font, Brushes.Red, x, y);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    e.Graphics.DrawString("Ошибка при загрузке изображения: " + ex.Message, font, Brushes.Red, x, y);
+                }
+            }
+        }
+    }
+}
